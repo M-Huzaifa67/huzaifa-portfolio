5293c7239a571398b560315035f9420b3a755e8e
M-Huzaifa67 <innocent.m.h@gmail.com>
2025-06-14 04:58:30 +0500

    now finally created and deploy to test on mobile

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+
+// module.exports = {
+//     // darkMode: 'class', // Enable dark mode support
+
+//     content: [
+//         './src/**/*.{html,js,css,vue,svelte,ts,tsx,jsx}',
+//         './index.html',
+//     ],
+
+//     theme: {
+//     colors: {
+//       'blue': '#1fb6ff',
+//       'purple': '#7e5bef',
+//       'pink': '#ff49db',
+//       'orange': '#ff7849',
+//       'green': '#13ce66',
+//       'yellow': '#ffc82c',
+//       'gray-dark': '#273444',
+//       'gray': '#8492a6',
+//       'gray-light': '#d3dce6',
+//     },
+//     fontFamily: {
+//       sans: ['Graphik', 'sans-serif'],
+//       serif: ['Merriweather', 'serif'],
+//     },
+//     extend: {
+//       backgroundColor: ['dark'],
+//       textColor: ['dark'],
+//       borderColor: ['dark'],
+//       display: ['dark'],
+//       screens: {
+//         'xs': '480px',
+//         'sm': '640px',
+//         'md': '768px',
+//         'lg': '1024px',
+//         'xl': '1280px',
+//         '2xl': '1536px',
+//         '3xl': '1920px',
+//       },
+//       spacing: {
+//         '8xl': '96rem',
+//         '9xl': '128rem',
+//       },
+//       borderRadius: {
+//         '4xl': '2rem',
+//       }
+//     }
+//   },
+
+// }
+
