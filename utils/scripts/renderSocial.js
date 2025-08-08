7abe8a9bb7cf83b58795ec0fdadd60bf746e5b83
M-Huzaifa67 <innocent.m.h@gmail.com>
2025-08-09 02:26:37 +0500

    set social icons

--- utils/scripts/renderSocial.js
+++ utils/scripts/renderSocial.js
@@ -1,25 +1,32 @@
 
 import { SOCIALS } from "/utils/socials.js";
 
-const createSocialLink = (social, index) => `
+const createSocialLink = (social, index) => {
+    const delay = index * 150; // Delay each skill slightly
+  return `
   <a 
     id="${social.id}" 
     href="${social.link}" 
     target="_blank" 
     class="${social.hoverColor} flex items-center"
-    data-aos="zoom-in"
-    data-aos-delay="${index * 150}"
-    data-aos-duration="700"
+     data-aos="fade-up" 
+      data-aos-duration="800" 
+      data-aos-delay="${delay}"
   >
     <i class="${social.icon}"></i>
     <span class="hidden md:block p-1">${social.title}</span>
   </a>
 `;
+}
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("socials-container");
-    if (container) {
-        container.innerHTML = SOCIALS.map(createSocialLink).join("");
-    }
+  const container = document.getElementById("socials-container");
+  if (container) {
+    // container.innerHTML = SOCIALS.map(createSocialLink).join("");
+    container.innerHTML = SOCIALS.map((social, index) => createSocialLink(social, index)).join('');
+    
+  }
 });
