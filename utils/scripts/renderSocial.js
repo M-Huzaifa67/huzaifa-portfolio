3ba26cd3690ae5b66af6f6cf821aacf67136f0d5
M-Huzaifa67 <innocent.m.h@gmail.com>
2025-08-10 23:07:20 +0500

    set meta image and animation

--- utils/scripts/renderSocial.js
+++ utils/scripts/renderSocial.js
@@ -2,21 +2,15 @@
 import { SOCIALS } from "/utils/socials.js";
 
 const createSocialLink = (social, index) => {
-    const delay = index * 150 + 100; // Delay each skill slightly
+  const delay = index * 150 + 100; // Delay each skill slightly
   return `
   <a 
     id="${social.id}" 
     href="${social.link}" 
     target="_blank" 
     class="${social.hoverColor} flex items-center"
-    data-aos="fade-up"
-    data-aos-offset="200"
-    data-aos-mirror="true"
-    data-aos-once="true"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-anchor-placement="bottom-bottom"
-      data-aos-delay="${delay}"
+    data-aos="fade-up" data-aos-mirror="true" data-aos-once="true" data-aos-duration="800" 
+    data-aos-easing="ease-in-out" data-aos-anchor-placement="bottom-bottom" data-aos-delay="${delay}"
   >
     <i class="${social.icon}"></i>
     <span class="hidden md:block p-1">${social.title}</span>
@@ -24,17 +18,16 @@ const createSocialLink = (social, index) => {
 `;
 }
 
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("socials-container");
-  if (!container) return;
-
-  container.innerHTML = SOCIALS.map((social, i) => createSocialLink(social, i)).join("");
-
+  if (container) {
+    container.innerHTML = SOCIALS.map((social, index) => createSocialLink(social, index)).join('');
+  }
   // Refresh AOS after dynamic content is added
   // if (window.AOS) {
   //   AOS.refresh();
   // }
 });
+
+
+
